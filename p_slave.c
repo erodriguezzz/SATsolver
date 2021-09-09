a897303368253300827b8121dcb513fb91331e6a
Ezequiel Rodriguez <learodriguez@itba.edu.ar>
2021-09-09 17:55:12 -0300

    keep working on slave's behaviour

--- p_slave.c
+++ p_slave.c
@@ -13,19 +13,20 @@
 #define WRITE 1
 
 int main(int argc, const char *argv[]) {
-    char buff[MAX_LENGTH], output[MAX_LENGTH], path[MAX_PATH_LENGTH];
-    sprintf(output, "%d\n", getpid());
+    char buff[MAX_LENGTH], output[MAX_LENGTH];
+    pid_t pid = getpid();
 
-    while(fgets(path, MAX_LENGTH, stdin) != NULL) {
-        sprintf(buff, "minisat %s |  grep -o -e \"Number of .*[0-9]\\+\" -e \"CPU time.*\" -e \".*SATISFIABLE\"", path);
-        FILE *stream = popen(buff, "r");
-        
+    while(fgets(buff, MAX_LENGTH, stdin) != NULL) {
+        sprintf(output, "minisat %s | grep -o -e \"Number of .*[0-9]\\+\" -e \"CPU time.*\" -e \".*SATISFIABLE\"", buff);
+        FILE *stream = popen(output, "r");
+        puts("Hola");
+        sprintf(output, "%d\n", pid);
         while (fgets(buff, MAX_LENGTH, stream) != NULL) {
             strcat(output, buff);
         }
         
         pclose(stream);
-        fputs(output, stdout);
+        printf(output);
     }
 
     return 0;
