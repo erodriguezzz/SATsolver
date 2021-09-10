fa6746df78b0c1ebbf53bd253a0bb1e54f429f81
BraveJero <jbrave271@gmail.com>
2021-09-10 15:51:57 -0300

    [wip] share memory.

--- p_master.c
+++ p_master.c
@@ -2,15 +2,16 @@
 
 
 #include <stdio.h>
-#include <sys/shm.h>
 #include <sys/ipc.h>
 #include <stdlib.h>
 
-#define SHM_KEY 1234
 #define FILES_PER_SLAVE 2
 
-int main(int argc, char const *argv[]){
-    if(argc == 1){
+int main(int argc, char const *argv[]) {
+    
+    struct shmseg *pShm;
+
+    if (argc == 1) {
         fprintf(stderr, "No files inputted. \n");
         exit(1);
     }
@@ -19,13 +20,15 @@ int main(int argc, char const *argv[]){
 
     int slaves = (argc - (argc % 2 ? 0 : 1)) / FILES_PER_SLAVE;
 
-    while(slaves != 0) {
+    while (slaves != 0) {
         
     }
     
 
     // -------------------------------
     //https://www.tutorialspoint.com/inter_process_communication/inter_process_communication_shared_memory.htm
+    
+    void * pShm = newShm(SHM_KEY, O_RDWR);
     
     
     return 0;
