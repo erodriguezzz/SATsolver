5503bca91f61181270ccd36a411fa742f21562d2
juan <juanigarcia@itba.edu.ar>
2021-09-12 21:19:45 -0300

    Finish basic master process structure.

--- p_master.c
+++ p_master.c
@@ -1,7 +1,8 @@
-#define _DEFAULT_SOURCE 
+#define _DEFAULT_SOURCE
 
 #include <stdio.h>
 #include <sys/shm.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
@@ -17,33 +18,60 @@ int main(int argc, char const *argv[]){
         fprintf(stderr, "No files inputted. \n");
         exit(1);
     }
-//    int n = (argc + (argc % 2)) / FILES_PER_SLAVE, files = 0;
-    int n = 1, m2s[2], s2m[2];
+    int n = (argc + FILES_PER_SLAVE - 2) /  FILES_PER_SLAVE, sent = 0, received = 0;
+    printf("Number of slaves: %d\n", n);
     slavesADT slaves = newSlaves();
-    if(slaves == NULL)
+    if(slaves == NULL) {
+        exit(1);
+    }
+    int m2s[2], s2m[2]; // Master to slave, slave to master.
+    if(pipe(s2m) < 0)
         exit(1);
     for(int i = 0; i < n; i++){
-
-        if(pipe(m2s) == -1 || pipe(s2m) == -1)
+        if(pipe(m2s) < 0)
             exit(1);
         int pid = fork();
         if(pid < 0)
             exit(1);
         if(pid == 0){
+            close(m2s[W]);
+            close(s2m[R]);
             dup2(m2s[R], STDIN_FILENO);
-            execlp("./p_slave", "p_slave", NULL);
+            dup2(s2m[W], STDOUT_FILENO);
+            execl("./p_slave", "p_slave", (char *) NULL);
+            return 1;
         }
         close(m2s[R]);
-        close(s2m[W]);
-        addSlave(slaves, i, s2m[R], m2s[W]);
+        if(addSlave(slaves, pid, m2s[W]) == 0) {
+            exit(1);
+        }
+        dprintf(m2s[W], "%s\n", argv[i + 1]);
+        sent++;
     }
-    char newline = '\n', eof = EOF;
-//    for(int i = 0; i < n; i++){
-//        int fd = getWriteFD(slaves, i);
-//        write(fd, argv[i + 1], PIPE_BUF);
-//        write(fd, &newline, 1);
-//    }
-    write(getWriteFD(slaves, 0), argv[1], PIPE_BUF);
-    write(getWriteFD(slaves, 0), &newline, 1);
+    close(s2m[W]);
+
+    FILE * s2m_ptr = fdopen(s2m[R], "r");
+    FILE * result = fopen("result.txt", "w+");
+    char *buf = NULL;
+    size_t len;
+
+    while(received < argc - 1) {
+        getline(&buf, &len, s2m_ptr);
+        fprintf(result, "%s", buf);
+        received++;
+        int id;
+        sscanf(buf, "%d", &id);
+        int fd = getWriteFD(slaves, id);
+        if (sent < argc - 1) {
+            dprintf(fd, "%s\n", argv[sent + 1]);
+            sent++;
+        } else {
+            close(fd);
+        }
+    }
+    free(buf);
+    fclose(s2m_ptr);
+    fclose(result);
+    freeSlaves(slaves);
     return 0;
 }
